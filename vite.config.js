85091e9d8952fd0ddc3ebb69766b1a48e9bc3a59
Mohit Pardhi <mpardhi11@gmail.com>
2024-01-20 15:44:24 +0530

    pages added

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://github.com/mpardhi11/dice-game/",
-  plugins: [react()],
-});
